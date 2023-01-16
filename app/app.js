535cae0e075a81ae4c099fc4427f8b652bd1ad39
OgutuJoseph <onyango.joseph@yahoo.com>
2023-01-16 13:41:08 +0300

    Seting up Dockerfile

--- app/app.js
+++ app/app.js
@@ -3,11 +3,12 @@ const app = express();
 require('dotenv').config();
 
 /** .env variables  */
-const port = process.env.PORT;
+// const port = process.env.PORT;
+const port = 5002;
 
 /** middlewares */
 app.use(express.urlencoded({ extended: false })) // to see bodies; not to have complicated bodies***
-app.use(express.json());
+app.use(express.json())
 
 app.listen(port, () => {
     console.log(`Server connected on port: ${port}`)
