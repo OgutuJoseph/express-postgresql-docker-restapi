0c7e7bffa77e4527209e315da109b57124188be1
OgutuJoseph <onyango.joseph@yahoo.com>
2023-01-23 15:11:20 +0300

    Project reset

--- test/app.js
+++
@@ -1,8 +0,0 @@
-const { expect } = require('chai');
-const app = require('../app/app');
-
-describe('Testing app creation', () => {
-    it('App is corretly exported', () => {
-        expect(typeof app).to.equal('function')
-    })
-});
