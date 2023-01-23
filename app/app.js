79e59036b40df76a84a3256b4019369e85dda90b
OgutuJoseph <onyango.joseph@yahoo.com>
2023-01-23 17:12:45 +0300

    Setup docker-compose and Dockerfile

--- app/app.js
+++ app/app.js
@@ -4,12 +4,16 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 /** middlewares */
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const heh = 3;
 /** routes */
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server has started on port: ${port}`)
-})
+});
+
+module.exports = app;
