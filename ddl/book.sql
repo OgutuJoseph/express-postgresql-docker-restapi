e795dad6eb853d957bd9fa18c455c962ff2954c5
OgutuJoseph <onyango.joseph@yahoo.com>
2023-01-25 16:54:12 +0300

    Interact with ddl

---
+++ ddl/book.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS book;
+
+CREATE TABLE book (
+    id CHARACTER(36) PRIMARY KEY,
+    author VARCHAR(255) NOT NULL,
+    price INTEGER NOT NULL,
+    description TEXT,
+    year_published INTEGER NOT NULL,
+    added_dttm TIMESTAMP DEFAULT NOW()
+)
